d7a42078f3d7ef72b5bcc9b33da55a01f49da086
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2022-02-13 19:04:26 -0600

    Iconos de los tabs personalizados

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
+
+  tabs = [
+    {
+      title: 'tab1',
+      active: false
+    },
+    {
+      title: 'tab2',
+      active: false
+    },
+    {
+      title: 'tab3',
+      active: false
+    }
+  ];
+
+
+
   constructor() {}
 
+
+  tabSelected( evento ){
+
+    this.tabs.forEach( tab =>{
+      if( tab.title === evento.tab ){
+        tab.active = true;
+      }else{
+        tab.active = false;
+      }
+    });
+    
+  }
 }
